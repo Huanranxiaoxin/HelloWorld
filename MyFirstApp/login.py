690fac5c07b7e30bee1a968d8c69b0c19ed23d32
huanranxiaoxin <cristiano.zheng@gmail.com>
2018-08-21 13:22:15 +0800

    test3

--- MyFirstApp/login.py
+++ MyFirstApp/login.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render,redirect
-from MyFirstApp.models import *
+from django.shortcuts import render, redirect
+from MyFirstApp.models import testusers
 from django.views.decorators import csrf
 from django.views.decorators.cache import cache_page
 
@@ -9,7 +8,7 @@ from django.views.decorators.cache import cache_page
 def login(request):
     ctx = {}
     try:
-        cookie_value = request.COOKIES.get('userinfo') # 读取cookie
+        cookie_value = request.COOKIES.get('userinfo')  # 读取cookie
         ctx['rlt'] = cookie_value
     except:
         ctx['rlt'] = '请登录'
@@ -21,16 +20,16 @@ def login_go(request):
     ctx = {}
     if request.POST:
         # 有数据
-        message = request.POST['accountName'] +','+ request.POST['password']
-        list = testusers.objects.filter(accountname = request.POST['accountName'], password = request.POST['password'])
+        list = testusers.objects.filter(
+            accountname=request.POST['accountName'], password=request.POST['password'])
         if list:
-            red = redirect('/login/') # 登录成功，重新定向
-            red.set_cookie('userinfo',list[0].accountname) # 将用户名写入cookie
-            return red # 去重定向页面
+            red = redirect('/login/')  # 登录成功，重新定向
+            red.set_cookie('userinfo', list[0].accountname)  # 将用户名写入cookie
+            return red  # 去重定向页面
         else:
-            message = '账号和密码错误，请重新登录吧'
+            ctx['rlt'] = '账号和密码错误，请重新登录吧'
+            return render(request, 'login.html', ctx)
     else:
         # 无数据
-        message = '请输入账户和密码'
-    ctx['rlt'] = message
-    return render(request, 'login.html', ctx)
+        ctx['rlt'] = '请输入账户和密码'
+        return render(request, 'login.html', ctx)
